d2a325783603b816be2c9d2483b2960f0851d9d1
Pransu pathak <pranpathak01@gmail.com>
2024-05-29 16:19:08 +0530

    complete hero

--- src/component/hero/Hero.tsx
+++ src/component/hero/Hero.tsx
@@ -11,28 +11,58 @@ const textVarients = {
     transition:{
       duration:1,
       staggerChildren: 0.1,
-    }
+    },
   },
- 
-
+    scrollButton:{
+      y:10,
+      opacity:0,
+      transition:{
+        duration:2,
+        repeat:Infinity,
+      },
+    }
+  
+}
+const sliderVarients = {
+  initial:{
+    delay:1,
+    x: "5%",
+  },
+  animate:{
+    x: "-100%",
+    transition:{
+      duration: 20, // Adjust duration as needed for smooth sliding
+      ease: "linear",
+      repeat: Infinity,
+    },
+  },
 }
 const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <motion.div className="textContainer" variants={textVarients}>
+        <motion.div 
+        className="textContainer" 
+        variants={textVarients} 
+        initial="initial"
+        animate="animate">
           <motion.h2 variants={textVarients}>HARVEY TYLER</motion.h2>
           <motion.h1 variants={textVarients}>Web developer and UI designer</motion.h1>
           <motion.div className="buttons" variants={textVarients}>
             <motion.button variants={textVarients}>See the Latest Works</motion.button>
             <motion.button variants={textVarients}>Contact me</motion.button>
           </motion.div>
-          <motion.img src="scroll.png" alt="" variants={textVarients}/>
+          <motion.img src="scroll.png" alt="" variants={textVarients} animate="scrollButton"/>
         </motion.div>
       </div>
-      <div className="slidingTextContainer">
+      <motion.div 
+      className="slidingTextContainer" 
+      variants={sliderVarients} 
+      initial="initial"
+      animate="animate"
+      >
         Writer Content Creator Influencer
-      </div>
+      </motion.div>
       <div className="imageContainer">
         <img src="/Sample.png" alt="" />
       </div>
